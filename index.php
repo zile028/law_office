3774cb6281e0516f8e6e431bfc33eccfa1f06715
zile028@gmail.com <zile028@gmail.com>
2021-03-15 21:40:17 +0100

    footer html scss

--- index.php
+++ index.php
@@ -64,7 +64,16 @@
             <p>SAT - 11:00/15:00</p>
         </article>
     </section>
-
+    <!-- map -->
+    <section class="map">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160600.5362536334!2d-114.22791612672587!3d51.02753956782558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2z0JrQsNC70LPQsNGA0LgsINCQ0LvQsdC10YDRgtCwLCDQmtCw0L3QsNC00LA!5e0!3m2!1ssr!2s!4v1615840419004!5m2!1ssr!2s"
+            width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </section>
+    <!-- footer -->
+    <footer>
+        <p>&copy; 2021. webcon.rs</p>
+    </footer>
 </body>
 
 </html>
