a1dadafddeb8402f29f838b7253de41fffa1c8ec
zile028@gmail.com <zile028@gmail.com>
2021-03-15 21:51:55 +0100

    servicies html

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php include "partials/main-header.php"?>
-<section>
-    <h1 style="text-align:center;font-size: 10rem;">Home</h1>
+<!-- servicies -->
+<section class="servicies container">
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Family law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
+
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Business law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
+
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Criminal law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
+
+    <article>
+        <i class="fas fa-landmark"></i>
+        <h3>Insurance law</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis,
+            facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium
+            cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+        <button>Learn more</button>
+    </article>
 </section>
 
 <?php include "partials/footer.php"?>
