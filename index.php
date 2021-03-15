7f5a41cb96fefdf7d1ccbe4310993b04f4264a51
zile028@gmail.com <zile028@gmail.com>
2021-03-15 21:16:35 +0100

    main header html and scss

--- index.php
+++ index.php
@@ -23,8 +23,24 @@
             <li><a href=""><i class="fab fa-instagram"></i></a></li>
         </ul>
     </section>
+    <!-- mainHeader -->
     <header class="mainHeader">
-
+        <nav class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <hgroup>
+            <h1>Experience - Trust - Results</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, ea. Fugit ab nihil eveniet accusamus
+                vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea
+                similique labore obcaecati? Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos
+                recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati?</p>
+            <button>view more</button>
+        </hgroup>
     </header>
 
 
