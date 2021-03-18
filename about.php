99924608b7ef6fd9dc296b0ad98fbb5fb47116a5
zile028@gmail.com <zile028@gmail.com>
2021-03-18 18:32:01 +0100

    about page

--- about.php
+++ about.php
@@ -1,6 +1,114 @@
 <?php include "partials/main-header.php"?>
-<section>
-    <h1 style="text-align:center;font-size: 10rem;">About</h1>
+
+<!-- peoples -->
+<section class="peoples">
+    <article class="container">
+        <article>
+            <img src="img/people1.jpg" alt="">
+            <div class="text">
+                <h3>Etta James</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.
+                </p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+        <article>
+            <img src="img/people2.jpg" alt="">
+            <div class="text">
+                <h3>Louis Armstrong</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.
+                </p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+        <article>
+            <img src="img/people3.jpg" alt="">
+            <div class="text">
+                <h3>Bessie Smith</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.
+                </p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+        <article>
+            <img src="img/people4.jpg" alt="">
+            <div class="text">
+                <h3>Elmore James</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro
+                    harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.
+                </p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </div>
+        </article>
+    </article>
 </section>
+<!-- personals -->
+<section class="personals">
+    <article class="container">
+        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, eius. Dolores impedit recusandae modi
+            blanditiis neque rerum architecto, nesciunt nobis, omnis voluptatem, cum eum? Culpa!</p>
+    </article>
+    <article class="container">
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+        <div>
+            <h4>Personal injury</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nam doloremque similique beatae rem magni,
+                culpa corporis blanditiis labore libero est enim animi rerum officia eveniet perspiciatis eaque
+                veritatis voluptatibus?</p>
+        </div>
+
+    </article>
+
+</section>
+
 
 <?php include "partials/footer.php"?>
