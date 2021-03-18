df1ef20ff6916f282b1f53432f3bc6f956ee4b8e
zile028@gmail.com <zile028@gmail.com>
2021-03-18 19:56:28 +0100

    blog page

--- blog.php
+++ blog.php
@@ -1,6 +1,68 @@
-<?php include "partials/main-header.php"?>
-<section>
-    <h1 style="text-align:center;font-size: 10rem;">Blog</h1>
-</section>
+<?php include "partials/blog-header.php"?>
+<main class="blog">
+    <section class="container">
+        <article>
+            <div class="posts">
+                <figure>
+                    <img src="img/about2.jpg" alt="">
+                </figure>
+                <div>
+                    <hgroup>
+                        <h2>Blog 1</h2>
+                        <p>Admin <span>18.03.2021.</span></p>
+                    </hgroup>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, perferendis dolore quae delectus
+                        hic
+                        modi vero eligendi velit officia est aliquam, explicabo accusamus in soluta inventore, quam
+                        architecto rem aspernatur.</p>
+                    <a href="">Read more &raquo;</a>
+                </div>
+            </div>
 
+            <div class="posts">
+                <figure>
+                    <img src="img/about.jpg" alt="">
+                </figure>
+                <div>
+                    <hgroup>
+                        <h2>Blog 2</h2>
+                        <p>Admin <span>18.03.2021.</span></p>
+                    </hgroup>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, perferendis dolore quae delectus
+                        hic
+                        modi vero eligendi velit officia est aliquam, explicabo accusamus in soluta inventore, quam
+                        architecto rem aspernatur.</p>
+                    <a href="">Read more &raquo;</a>
+                </div>
+            </div>
+
+            <div class="posts">
+                <figure>
+                    <img src="img/header.jpg" alt="">
+                </figure>
+                <div>
+                    <hgroup>
+                        <h2>Blog 3</h2>
+                        <p>Admin <span>18.03.2021.</span></p>
+                    </hgroup>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, perferendis dolore quae delectus
+                        hic
+                        modi vero eligendi velit officia est aliquam, explicabo accusamus in soluta inventore, quam
+                        architecto rem aspernatur.</p>
+                    <a href="">Read more &raquo;</a>
+                </div>
+            </div>
+        </article>
+        <aside>
+            <h2>Categories</h2>
+            <ul>
+                <li><a href="">Category 1</a></li>
+                <li><a href="">Category 2</a></li>
+                <li><a href="">Category 3</a></li>
+                <li><a href="">Category 4</a></li>
+                <li><a href="">Category 5</a></li>
+            </ul>
+        </aside>
+    </section>
+</main>
 <?php include "partials/footer.php"?>
